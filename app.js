31eab3fc6f515917c2e9426e2e2baea6e23b42e4
Abdulrhaman-Mohamed <abdorrr2015@gmail.com>
2024-03-11 00:03:14 +0200

    first commit

---
+++ app.js
@@ -0,0 +1,44 @@
+const express = require("express");
+const dotenv = require("dotenv");
+dotenv.config();
+const app = express();
+const connect = require("./dataBase/connectToDB");
+const PORT = process.env.PORT || 5000;
+
+app.use(express.json());
+
+const cors = require('cors');
+app.use(cors());
+const authRoute = require("./Routes/authRoute");
+app.use("/users", authRoute);
+
+
+const User = require("./models/userSchema");
+app.get("/user", async (req, res) => {
+  try {
+    const users = await User.find();
+    res.json(users);
+  } catch (eror) {
+    console.log(eror);
+    res.status(500).json({
+      message: "Internal Server Error",
+    });
+  }
+});
+app.get("/test", (req, res) => {
+  res.json({
+    message: "Hello",
+  });
+});
+
+//cors to can access atals database link
+app.get("/test", (req, res) => {
+  res.json({
+    message: "Hello",
+  });
+});
+
+connect();
+app.listen(PORT, () => {
+  console.log(PORT, "server is running..");
+});
---
+++ models/userSchema.js
@@ -0,0 +1,31 @@
+const mongoose = require("mongoose");
+const Joi = require("joi");
+const { joiPasswordExtendCore } = require("joi-password");
+const JoiPassword = Joi.extend(joiPasswordExtendCore);
+
+const userSchema = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    gender:{
+        type:String,
+        enum:["male","female"]
+    }
+  },
+  { timestamps: true }
+);
+
+
+
+module.exports = mongoose.model("user", userSchema);
